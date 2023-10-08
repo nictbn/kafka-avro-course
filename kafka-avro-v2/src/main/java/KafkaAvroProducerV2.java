97f79aea55dd2c46fe1346068951d2727f3c85d0
Nicu Taban <nictbndeveloper@gmail.com>
2023-10-08 18:49:45 +0300

    Writing the V2 Kafka Avro Producer

---
+++ kafka-avro-v2/src/main/java/KafkaAvroProducerV2.java
@@ -0,0 +1,45 @@
+import com.example.Customer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaAvroProducerV2 {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("acks", "1");
+        properties.setProperty("retries", "10");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
+        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+
+        String topic = "customer-avro";
+        KafkaProducer<String, Customer> kafkaProducer = new KafkaProducer<>(properties);
+
+        Customer customer = Customer.newBuilder()
+                .setFirstName("Mark")
+                .setLastName("Johnson")
+                .setAge(26)
+                .setHeight(160f)
+                .setWeight(90f)
+                .setPhoneNumber("123-467-980")
+                .setEmail("mark.johnson@gmail.com")
+                .build();
+
+        ProducerRecord<String, Customer> producerRecord = new ProducerRecord<>(topic, customer);
+        kafkaProducer.send(producerRecord, (recordMetadata, e) -> {
+            if (e == null) {
+                System.out.println("Success");
+                System.out.println(recordMetadata.toString());
+            } else {
+                e.printStackTrace();
+            }
+        });
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+}
